087d748dd17ea9318ad7bb136282b64c16e23fbf
s5255384 <100850996+s5255384@users.noreply.github.com>
2022-03-16 09:17:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,19 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
+          float a; float b; float c;
+          float Atriangolo=((a*b)/2);
+          float Aquadrato=(a*a);
+          float Arettangolo=(a*b);
+          float Atrapezio=(((a+b)*c)/2);
 
 cin >> a >> b >> c;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
+          std::cout<<((a*b)/2)<<std::endl;
+          std::cout<<(a*a)<<std::endl;
+          std::cout<<(a*b)<<std::endl;
+          std::cout<<(((a+b)*c)/2)<<std::endl; 
+ return 0;
 
 
 }
